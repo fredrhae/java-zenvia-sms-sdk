4b062daa78ceda392f00c5d3e0ce00812cfb2a00
Fred Rhae <fredrhae@gmail.com>
2017-07-29 23:24:45 -0300

    More tests added
    Now, it's time to implement the other functionalities.

--- src/test/java/com/zenvia/sms/sdk/api/ZenviaSmsTest.java
+++ src/test/java/com/zenvia/sms/sdk/api/ZenviaSmsTest.java
@@ -7,6 +7,7 @@ import com.zenvia.sms.sdk.base.responses.SendSmsResponse;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.net.URI;
 import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
@@ -15,8 +16,9 @@ import static org.junit.Assert.assertEquals;
  *
  */
 public class ZenviaSmsTest {
-    private final String AUTH_KEY = "base64encodingkey";
-    private final String PHONE_NUMBER = "555199999999";
+    private final String AUTH_KEY = "authkey";
+    private final String PHONE_NUMBER = "55519999999";
+    private final String ENDPOINT = "https://api-rest.zenvia360.com.br/services";
 
     private ZenviaSms zenviaSms;
     @Before
@@ -26,40 +28,57 @@ public class ZenviaSmsTest {
 
     @Test
     public void setEndpoint() throws Exception {
+        zenviaSms.setEndpoint(new URI("https://api-rest.zenvia360.com.br/services/test"));
+        assertEquals(this.ENDPOINT + "/test", zenviaSms.getEndpoint().toString());
+        zenviaSms.setEndpoint(new URI(ENDPOINT));
     }
 
     @Test
     public void setBasicAuthorizationKey() throws Exception {
+        zenviaSms.setBasicAuthorizationKey(this.AUTH_KEY);
+        assertEquals(this.AUTH_KEY, zenviaSms.getBasicAuthorizationKey());
     }
 
     @Test
     public void zenviaSendSmsUrl() throws Exception {
+        assertEquals("https://api-rest.zenvia360.com.br/services/send-sms",
+                zenviaSms.zenviaSendSmsUrl().toString());
     }
 
     @Test
     public void zenviaSendSmsMultipleUrl() throws Exception {
+        assertEquals("https://api-rest.zenvia360.com.br/services/send-sms-multiple",
+                zenviaSms.zenviaSendSmsMultipleUrl().toString());
     }
 
     @Test
     public void zenviaGetSmsStatusUrl() throws Exception {
+        assertEquals("https://api-rest.zenvia360.com.br/services/get-sms-status/test-id",
+                zenviaSms.zenviaGetSmsStatusUrl("test-id").toString());
     }
 
     @Test
     public void zenviaListReceivedSmsUrl() throws Exception {
+        assertEquals("https://api-rest.zenvia360.com.br/services/received/list",
+                zenviaSms.zenviaListReceivedSmsUrl().toString());
     }
 
     @Test
     public void zenviaListReceivedSmsByPeriodUrl() throws Exception {
+        assertEquals("https://api-rest.zenvia360.com.br/services/received/search/test-date/test-date",
+                zenviaSms.zenviaListReceivedSmsByPeriodUrl("test-date","test-date").toString());
     }
 
     @Test
     public void zenviaCancelSmsUrl() throws Exception {
+        assertEquals("https://api-rest.zenvia360.com.br/services/cancel-sms/test-id",
+                zenviaSms.zenviaCancelSmsUrl("test-id").toString());
     }
 
     @Test
     public void sendSingleSms() throws Exception {
         SendSmsRequest smsRequest = SendSmsRequest.builder()
-                                        .from("Sdk test")
+                                        .from("Frederico Leal")
                                         .to(PHONE_NUMBER)
                                         .msg("Teste de envio!")
                                         .id("test-" + new Random().nextInt(100))
@@ -67,7 +86,7 @@ public class ZenviaSmsTest {
                                         .build();
 
         SendSmsResponse response = zenviaSms.sendSingleSms(smsRequest);
-        assertEquals(response.getStatusCode(), SmsStatusCode.OK);
+        assertEquals(SmsStatusCode.OK, response.getStatusCode());
     }
 
 }
